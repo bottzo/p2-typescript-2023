00beb1e7f01d8c25110902ae61c5532f3820cef3
bottzo <jordiet1999@gmail.com>
2023-04-21 23:12:12 +0200

    printing the json fetched

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-fetch("https://randompokemon.com/dex/all.json").then((r)=>console.log(r));
+fetch("https://randompokemon.com/dex/all.json").then(function (response){
+    return response.json();
+}).then(function (obj){
+    console.log(obj);
+}).catch(function(error){
+    console.error("smth went wrong when fetching the url or with the json response parsing");
+    console.log(error);
+});
