9aad596355a6f368f9aa870bf4587c0fe2f38871
bottzo <jordiet1999@gmail.com>
2023-04-22 12:50:31 +0200

    writing all pokemons to a file in json format

--- js/test.js
+++ js/test.js
@@ -1,3 +1,5 @@
+import {writeFile} from "fs/promises";
+
 function pokemon(id,name,types) {
     this.id = id;
     this.name = name;
@@ -22,10 +24,24 @@ async function loadPokemons()
     return pokemons;
 }
 
+//request 1 pokemon and create an html page with the image!!!
+function wrietPokemons(pokemons){
+    let data = '[';
+    for(pokemon of pokemons)
+    {
+        data += `{${pokemon.id},${pokemon.name},${pokemon.types}},`
+    }
+    //removing last coma
+    data = data.substring(0,data.length-1);
+    data += ']';
+    writeFile("pokemons.json", data);
+}
+
 loadPokemons().then(function(pokemons){
+wrietPokemons(pokemons);
 console.log(pokemons[35].name);
 console.log(pokemons[35].getPngName());
 }).catch(function(error){
     console.error("smth went wrong when fetching the url or with the json response parsing");
     console.log(error);
-})
+});
